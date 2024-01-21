7e584a42752c7f49394244d15cb3a03143e1d71d
unknown <annton.astashkin@gmail.com>
2024-01-22 00:07:26 +0600

    init

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+};
+
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+// let assetPrefix = ''
+// let basePath = '/'
+
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
+
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   images: {
+//     loader: 'imgix',
+//     path: 'the "domain" of your Imigix source',
+//   },
+// }
+
+export default nextConfig;
